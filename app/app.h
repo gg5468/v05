58bbcc10d905c3e198dc0ab40c8fe65031913161
gg307 <gg307@email.com>
2024-12-31 18:35:18 +0100

    add

--- app/app.h
+++ app/app.h
@@ -5,4 +5,36 @@
 namespace vsite::oop::v5 
 {
 
+    double operator"" _inch(unsigned long long num);
+    double operator"" _eur(unsigned long long eu);
+
+    class product {
+        double p;
+    public:
+        product(double val) : p(val) {};
+        double price();
+    };
+
+
+    class vehicle {
+        int y;
+    public:
+        vehicle(int val) : y(val) {};
+        int year();
+    };
+
+    class car : public product, public vehicle {
+        tire t;
+    public:
+        car(int y, double p, double d) : vehicle(y), product(p), t(d) {};
+
+        double tire_diameter();
+    };
+
+    class tire {
+        double d;
+    public:
+        tire(double val) : d(val) {};
+        double diameter();
+    };
 }
